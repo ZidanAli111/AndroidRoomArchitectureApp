77395e82f2a9145aa84a5d6650ae636b3f84659c
ZIDAN <zidanali111@gmail.com>
2022-01-02 22:04:04 +0530

    RecyclerView added.

--- app/src/main/java/com/example/roomwordsample/Word.java
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-/*
+/**
 @Entity(tableName = "word_table") Each @Entity class represents a SQLite table.
 Annotate your class declaration to indicate that it's an entity.
 You can specify the name of the table if you want it to be different from the name of the class.
@@ -14,28 +14,26 @@ This names the table "word_table".
 @Entity(tableName = "word_table")
 public class Word {
 
-    /*
+
+    /**
     @PrimaryKey Every entity needs a primary key. To keep things simple, each word acts as its own primary key.
     @NonNull Denotes that a parameter, field, or method return value can never be null.
     @ColumnInfo(name = "word") Specify the name of the column in the table if you want it to be
      different from the name of the member variable.
      */
-    @PrimaryKey(autoGenerate = true)
-    private int id;
 
-    @NonNull
-    @ColumnInfo(name = "word")
-    private String mWord;
+        @PrimaryKey
+        @NonNull
+        @ColumnInfo(name = "word")
+        private String mWord;
 
+        public Word(@NonNull String word) {
+            this.mWord = word;
+        }
 
-    public Word(@NonNull String word) {
-        this.mWord = word;
+        @NonNull
+        public String getWord() {
+            return this.mWord;
+        }
     }
 
-    /* Every field that's stored in the database needs to be either public or have a "getter" method.
-        This sample provides a getWord() method.
-     */
-    public String getmWord() {
-        return mWord;
-    }
-}
